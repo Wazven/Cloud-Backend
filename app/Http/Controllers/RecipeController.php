d6bebcccb637b9ed8570cf798def5d94b6ad5546
Aldi Rhiyadi <115007280+Wazven@users.noreply.github.com>
2023-06-02 16:37:27 +0000

    fix recipe controller

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -8,30 +8,11 @@ use Illuminate\Support\Facades\Validator;
 
 class RecipeController extends Controller
 {
-    public function index(Request $request){
-
-        $data = Validator::make($request->all(), [
-            'page' => 'required|integer',
-            'size' => 'required|integer'
-        ]);
-
-        if($data->fails()){
-            return response([
-                'message' => 'Failed',
-                'data' => $data->errors()
-            ], 400);
-        }
-
-        $page = $request->page;
-        $size = $request->size;
-
-        $recipe = recipe::skip(($page-1)*$size)->take($size)->get();
-        
-        // $recipe = recipe::where('name','=','Sauteed Bananas with Cardamom Praline Sauce')->get();
-        
+    public function index(){
+        $recipe = Recipe::all();
         if(count($recipe) > 0){
             return response([
-                'message' => 'Retrieve All Success',
+
                 'data' => $recipe
             ], 200);
         }
@@ -39,7 +20,7 @@ class RecipeController extends Controller
         return response([
             'message' => 'Empty',
             'data' => null
-        ], 400);
+        ], 400); 
     }
-    
+
 }
